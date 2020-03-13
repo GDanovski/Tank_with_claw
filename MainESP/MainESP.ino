2a2076ddf988476bbe4bf8a7ac441ae3d9c30c07
Georgi Danovski <georgi_danovski@abv.bg>
2020-03-13 14:03:26 +0200

    Tank_with_claw

---
+++ MainESP/MainESP.ino
@@ -0,0 +1,438 @@
+//GDanovski's tank code
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+///////////////////////////////////////////////////////////////
+//BLE variables
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+std::string output = ";";
+String input = "";
+String current = "";
+int ind = 0;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID           "b8c78f30-57ae-11ea-82b4-0242ac130003" 
+#define CHARACTERISTIC_UUID "c1569f89-7936-4a43-9cf1-0f9690223454"
+////////////////////////////////////////////////////////////////
+//setup uART ports
+#define RXD0 3
+#define TXD0 1
+#define RXD1 16
+#define TXD1 17
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//GPIO indexing
+const int M1A = 32;
+const int M1B = 33;
+const int M2A = 25;
+const int M2B = 26;
+
+const int S1 = 27;
+const int S2 = 14;
+const int S3 = 12;
+const int S4 = 13;
+
+const int LED = 2;
+
+const int RX1 = 3;
+const int TX1 = 1;
+const int RX2 = 16;
+const int TX2 = 17;
+//PWN channels
+const int M1A_channel = 1;
+const int M1B_channel = 2;
+const int M2A_channel = 3;
+const int M2B_channel = 4;
+
+const int S1_channel = 5;
+const int S2_channel = 6;
+const int S3_channel = 7;
+const int S4_channel = 8;
+   
+// setting PWM properties
+const int Servo_freq = 50;
+const int Motor_freq = 500;    
+const int resolution = 10;//max 1024
+//Borders
+const int M_min = 0;
+
+const int S1_min = 30;
+const int S2_min = 75;
+const int S3_min = 60;
+const int S4_min = 30;
+
+const int M_max = 1024;
+
+const int S1_max = 125;
+const int S2_max = 120;
+const int S3_max = 110;
+const int S4_max = 100;
+    
+//Variables
+int MotorsStatus = 5;
+
+int S1_value = 0;
+int S2_value = 0;
+int S3_value = 0;
+int S4_value = 0;
+
+int M_speed = 0;
+///////////////////////////////////////////////////////////
+//My methods
+//Functions
+void ConfigChannels(){
+  // initialize digital pin LED as an output.
+  Serial1.println("Starting pin configuration...");
+  pinMode(LED, OUTPUT);    
+  digitalWrite(LED, LOW); 
+  // configure LED PWM functionalitites
+  pinMode(M1A, OUTPUT);
+  pinMode(M1B, OUTPUT);
+  pinMode(M2A, OUTPUT);
+  pinMode(M2B, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(S4, OUTPUT);
+  
+  ledcSetup(M1A_channel, Motor_freq, resolution);
+  ledcSetup(M1B_channel, Motor_freq, resolution);
+  ledcSetup(M2A_channel, Motor_freq, resolution);
+  ledcSetup(M2B_channel, Motor_freq, resolution);
+
+  ledcSetup(S1_channel, Servo_freq, resolution);
+  ledcSetup(S2_channel, Servo_freq, resolution);
+  ledcSetup(S3_channel, Servo_freq, resolution);
+  ledcSetup(S4_channel, Servo_freq, resolution);
+  
+   // attach the channel to the GPIO to be controlled
+   ledcAttachPin(M1A, M1A_channel);
+   ledcAttachPin(M1B, M1B_channel);
+   ledcAttachPin(M2A, M2A_channel);
+   ledcAttachPin(M2B, M2B_channel);
+
+   ledcAttachPin(S1, S1_channel);
+   ledcAttachPin(S2, S2_channel);
+   ledcAttachPin(S3, S3_channel);
+   ledcAttachPin(S4, S4_channel);
+
+   //setup initial values
+   ledcWrite(M1A_channel,  M_min);
+   ledcWrite(M1B_channel,  M_min);
+   ledcWrite(M2A_channel,  M_min);
+   ledcWrite(M2B_channel,  M_min);
+
+   ledcWrite(S1_channel,  S1_value);
+   ledcWrite(S2_channel,  S2_value);
+   ledcWrite(S3_channel,  S3_value);
+   ledcWrite(S4_channel,  S4_value);
+}
+void DecodeInput(){
+  if(!CheckSizeOfInput()){ 
+    Serial1.println("Error: decoding!");   
+    return;
+  }
+  // MotorsSpeed | MotorsStatus | Servo1 | Servo2 | Servo3|Servo4|
+  //reset
+  current = "";
+  ind = 0;
+  int curValue = 0;
+  //decode values
+   if (input.length() > 0) {        
+      for (int i = 0; i < input.length(); i++){
+        if(input[i]=='|'){
+          curValue = current.toInt();
+          current = "";
+          switch(ind){
+            case 0:
+            //MotorSpeed
+              if(M_speed!=curValue){
+                M_speed = curValue;
+                if(M_speed>M_max){
+                  M_speed = M_max;
+                }
+                SetMotors();
+              }
+              break;
+            case 1:
+            //MotorsStatus
+              if(MotorsStatus!=curValue){
+                MotorsStatus = curValue;
+                SetMotors();
+              }
+              break;
+            case 2:
+              //S1
+              if(curValue != S1_value){
+              if(curValue<S1_min){
+                S1_value=S1_min;
+              }
+              else if(curValue>S1_max){
+                S1_value=S1_max;
+              }
+              else{
+                S1_value=curValue;
+              }
+              ledcWrite(S1_channel,  S1_value);
+              }
+              break;
+            case 3:
+              //S2
+              if(curValue != S2_value){
+              if(curValue<S2_min){
+                S2_value=S2_min;
+              }
+              else if(curValue>S2_max){
+                S2_value=S2_max;
+              }
+              else{
+                S2_value=curValue;
+              }
+              ledcWrite(S2_channel,  S2_value);
+              }
+              break;
+            case 4:
+              //S3
+              if(curValue != S3_value){
+              if(curValue<S3_min){
+                S3_value=S3_min;
+              }
+              else if(curValue>S3_max){
+                S3_value=S3_max;
+              }
+              else{
+                S3_value=curValue;
+              }
+              ledcWrite(S3_channel,  S3_value);
+              }
+              break;
+            case 5:
+              //S4     
+              if(curValue != S4_value ){         
+              if(curValue<S4_min){
+                S4_value=S4_min;
+              }
+              else if(curValue>S4_max){
+                S4_value=S4_max;
+              }
+              else{
+                S4_value=curValue;
+              }
+              ledcWrite(S4_channel,  S4_value);
+              }
+              break;
+          }
+          ind++;
+        }
+        else{  
+          current+=input[i]; 
+        }
+      }
+   }
+   //clear
+   current = "";
+    ind = 0;
+}
+void SetMotors(){
+  switch(MotorsStatus){
+    case 1:
+      ledcWrite(M1A_channel,  M_min);
+      delay(10);
+      ledcWrite(M1B_channel,  M_speed);
+      ledcWrite(M2A_channel,  M_min);
+      ledcWrite(M2B_channel,  M_min);
+      break;
+    case 2:
+      ledcWrite(M1A_channel,  M_min);
+      delay(10);
+      ledcWrite(M1B_channel,  M_speed);
+      ledcWrite(M2A_channel,  M_min);
+      delay(10);
+      ledcWrite(M2B_channel,  M_speed);
+      break;
+    case 3:
+      ledcWrite(M1A_channel,  M_min);
+      ledcWrite(M1B_channel,  M_min);
+      ledcWrite(M2A_channel,  M_min);
+      delay(10);
+      ledcWrite(M2B_channel,  M_speed);
+      break;
+    case 4:
+      ledcWrite(M1A_channel,  M_min);
+      delay(10);
+      ledcWrite(M1B_channel,  M_speed);
+      ledcWrite(M2B_channel,  M_min);
+      delay(10);
+      ledcWrite(M2A_channel,  M_speed);
+      break;
+    case 5:
+      ledcWrite(M1A_channel,  M_min);
+      ledcWrite(M1B_channel,  M_min);
+      ledcWrite(M2A_channel,  M_min);
+      ledcWrite(M2B_channel,  M_min);
+      break;
+    case 6:
+      ledcWrite(M1B_channel,  M_min);
+      delay(10);
+      ledcWrite(M1A_channel,  M_speed);
+      ledcWrite(M2A_channel,  M_min);
+      delay(10);
+      ledcWrite(M2B_channel,  M_speed);
+      break;
+    case 7:
+      ledcWrite(M1B_channel,  M_min);
+      delay(10);
+      ledcWrite(M1A_channel,  M_speed);
+      ledcWrite(M2A_channel,  M_min);
+      ledcWrite(M2B_channel,  M_min);
+      break;
+    case 8:
+      ledcWrite(M1B_channel,  M_min);
+      delay(10);
+      ledcWrite(M1A_channel,  M_speed);
+      ledcWrite(M2B_channel,  M_min);
+      delay(10);
+      ledcWrite(M2A_channel,  M_speed);
+      break;
+    case 9:
+      ledcWrite(M1A_channel,  M_min);
+      ledcWrite(M1B_channel,  M_min);
+      ledcWrite(M2B_channel,  M_min);
+      delay(10);
+      ledcWrite(M2A_channel,  M_speed);
+      break;
+  }
+}
+bool CheckSizeOfInput(){
+
+if (input.length() == 0){return false;}
+int count = 0;
+ for (int i = 0; i < input.length(); i++){
+        if(input[i] == '|'){ count++; }      
+   }
+   if(count == 6){
+    return true;
+   }
+   else{
+    return false;
+   }
+}
+void StopMotors(){
+  MotorsStatus = 5;
+  ledcWrite(M1A_channel,  M_min);
+  ledcWrite(M1B_channel,  M_min);
+  ledcWrite(M2A_channel,  M_min);
+  ledcWrite(M2B_channel,  M_min);
+}
+////////////////////////////////////////////////////////////////
+//BLE Methods
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+
+      if (value.length() > 0) {
+      for (int i = 0; i < value.length(); i++){
+         if(value[i] == ';'){     
+          DecodeInput();
+          //Serial.println("Recieved: " + input);
+          //Serial1.println("Recieved: " + input);
+          input = "";
+         }
+         else{
+          input+=value[i];          
+         }
+      }
+    }
+    }
+};
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      digitalWrite(LED, HIGH);
+      
+      Serial1.println("Connected");
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      digitalWrite(LED, LOW);
+      
+      Serial1.println("Disconnected");
+    }
+};
+void BLEServerSetUp(){
+  Serial1.println("Starting BLE server!");
+  // Create the BLE Device
+  BLEDevice::init("GDanovski's tank");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->setValue("Welcome, I'm ready to rumble!");
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial1.println("Waiting a client connection to notify...");
+}
+/////////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  Serial1.begin(115200, SERIAL_8N1, RXD0, TXD0);
+  //Serial2.begin(115200, SERIAL_8N1, RXD1, TXD1);
+  Serial1.println("Setup GDanovski's tank");
+  //setup GPIO
+  ConfigChannels();
+  //setup server
+  BLEServerSetUp();  
+}
+
+void loop() {
+  //read uART1
+ // while (Serial1.available()) {
+  //  Serial.print(char(Serial1.read()));
+  //}
+  // notify changed value
+  if (deviceConnected) {
+      pCharacteristic->setValue(output);
+      pCharacteristic->notify();
+      delay(3); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+  }
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+     delay(500); // give the bluetooth stack the chance to get things ready
+     pServer->startAdvertising(); // restart advertising
+     Serial1.println("start advertising");
+     oldDeviceConnected = deviceConnected;
+     StopMotors();
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+     // do stuff here on connecting
+     oldDeviceConnected = deviceConnected;
+  }
+  delay(100);
+}
---
+++ RemoteControl/RemoteControl.ino
@@ -0,0 +1,319 @@
+//GDanovski's tank remote
+
+#include "BLEDevice.h"
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("b8c78f30-57ae-11ea-82b4-0242ac130003");
+// The characteristic of the remote service we are interested in.
+static BLEUUID    charUUID("c1569f89-7936-4a43-9cf1-0f9690223454");
+
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLEAdvertisedDevice* myDevice;
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//setup uART ports
+#define RXD0 3
+#define TXD0 1
+#define RXD1 16
+#define TXD1 17
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//my pins
+const int S1 = 36;
+const int S2 = 39;
+const int S3 = 34;
+const int S4 = 35;
+const int M1 = 33;
+const int M2 = 32;
+const int LED = 2;
+//values
+
+const double S1_min = 30;
+const double S2_min = 75;
+const double S3_min = 60;
+const double S4_min = 30;
+
+const double S1_max = 125;
+const double S2_max = 120;
+const double S3_max = 110;
+const double S4_max = 100;
+
+double S1_Const = 4095;
+double S2_Const = 4095;
+double S3_Const = 4095;
+double S4_Const = 4095;
+double M_Const = 4095;
+
+const double Potentiometer_Const = 4095;
+//Variables
+int MotorsStatus = 5;
+
+int S1_value = 0;
+int S2_value = 0;
+int S3_value = 0;
+int S4_value = 0;
+
+int M_speed_value = 1024;
+int M1_value = 0;
+int M2_value = 0;
+//temp variable
+double currentValue = 0;
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//my methods
+void Setup_Potentiometer(){
+  S1_Const = (S1_max-S1_min)/Potentiometer_Const;
+  S2_Const = (S2_max-S2_min)/Potentiometer_Const;
+  S3_Const = (S3_max-S3_min)/Potentiometer_Const;
+  S4_Const = (S4_max-S4_min)/Potentiometer_Const;
+  
+  //configurate pins
+  pinMode (S1, INPUT); 
+  pinMode (S2, INPUT); 
+  pinMode (S3, INPUT); 
+  pinMode (S4, INPUT); 
+  pinMode (M1, INPUT); 
+  pinMode (M2, INPUT);  
+  
+  pinMode (LED, OUTPUT);
+  digitalWrite(LED, LOW);  
+}
+int ReadPotentiometer(int Pin, double Const,double Min, int OldValue){
+  // Reading potentiometer value
+  currentValue = analogRead(Pin);  
+  currentValue = Min+(currentValue*Const);
+  
+  if(OldValue+2 < (int)currentValue || OldValue-2 > (int)currentValue){
+    OldValue = (int)currentValue;    
+  }
+  
+  return OldValue;
+}
+int CheckMotorIndex(int M_value){
+  if(M_value<1650){
+    return -1;
+  }
+  else if (M_value>2150){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+void CalcMotorSpeed(){
+  double mid = 2047;
+  double M1_speed = abs(M1_value - mid);
+  double M2_speed = abs(M2_value - mid);
+  
+  if(M1_speed<M2_speed)
+  {
+    M1_speed = M2_speed;
+  }
+  
+  M1_speed = M1_speed/2;
+  
+  if(M1_speed<1000){
+    M1_speed = 350;
+  }
+  
+  if(M_speed_value+2 < (int)M1_speed || M_speed_value-2 > (int)M1_speed){
+    M_speed_value = (int)M1_speed;    
+  }
+}
+void MeasureValues(){
+  //read analog values
+  S1_value = ReadPotentiometer(S1, S1_Const, S1_min, S1_value);
+  S2_value = ReadPotentiometer(S2, S2_Const, S2_min, S2_value);
+  S3_value = ReadPotentiometer(S3, S3_Const, S3_min, S3_value);
+  S4_value = ReadPotentiometer(S4, S4_Const, S4_min, S4_value);
+ 
+  M1_value = analogRead(M1);
+  M2_value = analogRead(M2);
+  CalcMotorSpeed();
+  //check motor status
+  int M1_index = CheckMotorIndex(M1_value);
+  int M2_index = CheckMotorIndex(M2_value);
+  String M_string = String(String(M1_index)+"|"+String(M2_index));
+
+  if (M_string == "-1|-1"){
+    MotorsStatus = 1;
+  }
+  else if (M_string == "-1|0"){
+    MotorsStatus = 2;
+  }
+  else if (M_string == "-1|1"){
+    MotorsStatus = 3;
+  }
+  else if (M_string == "0|-1"){
+    MotorsStatus = 6;
+  }
+  else if (M_string == "0|0"){
+    MotorsStatus = 5;
+  }
+  else if (M_string ==  "0|1"){
+    MotorsStatus = 4;
+  }
+  else if (M_string == "1|-1"){
+    MotorsStatus = 7;
+  }
+  else if (M_string == "1|0"){
+    MotorsStatus = 8;
+  }
+  else if (M_string ==  "1|1"){
+    MotorsStatus = 9;
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//BLE methods
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {   
+    Serial1.print("Notify callback for characteristic ");
+    Serial1.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+    Serial1.print(" of data length ");
+    Serial1.println(length);
+    Serial1.print("data: ");
+    Serial1.println((char*)pData);
+}
+
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {
+    digitalWrite(LED, HIGH);
+    Serial1.println("Connected");
+  }
+
+  void onDisconnect(BLEClient* pclient) {
+    connected = false;
+    digitalWrite(LED, LOW);
+    Serial1.println("Disconnected");
+  }
+};
+
+bool connectToServer() {
+    Serial1.print("Forming a connection to ");
+    Serial1.println(myDevice->getAddress().toString().c_str());
+    
+    BLEClient*  pClient  = BLEDevice::createClient();
+    Serial1.println(" - Created client");
+
+    pClient->setClientCallbacks(new MyClientCallback());
+
+    // Connect to the remove BLE Server.
+    pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+    Serial1.println(" - Connected to server");
+
+    // Obtain a reference to the service we are after in the remote BLE server.
+    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr) {
+      Serial1.print("Failed to find our service UUID: ");
+      Serial1.println(serviceUUID.toString().c_str());
+      pClient->disconnect();
+      return false;
+    }
+    Serial1.println(" - Found our service");
+
+    // Obtain a reference to the characteristic in the service of the remote BLE server.
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+    if (pRemoteCharacteristic == nullptr) {
+      Serial1.print("Failed to find our characteristic UUID: ");
+      Serial1.println(charUUID.toString().c_str());
+      
+      pClient->disconnect();
+      return false;
+    }
+    Serial1.println(" - Found our characteristic");
+
+    // Read the value of the characteristic.
+    if(pRemoteCharacteristic->canRead()) {
+      std::string value = pRemoteCharacteristic->readValue();
+      Serial1.print("The characteristic value was: ");
+      Serial1.println(value.c_str());
+    }
+
+    if(pRemoteCharacteristic->canNotify())
+      pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+    connected = true;
+}
+/**
+ * Scan for BLE servers and find the first one that advertises the service we are looking for.
+ */
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+ /**
+   * Called for each advertising BLE server.
+   */
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial1.print("BLE Advertised Device found: ");
+    Serial1.println(advertisedDevice.toString().c_str());
+
+    // We have found a device, let us now see if it contains the service we are looking for.
+    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+
+      BLEDevice::getScan()->stop();
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+      doConnect = true;
+      doScan = true;
+
+    } // Found our server
+  } // onResult
+}; // MyAdvertisedDeviceCallbacks
+void SetUpBLEClient(){
+  BLEDevice::init("GDanovski's tank remote");
+
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+  Serial1.begin(115200, SERIAL_8N1, RXD0, TXD0);
+  //Serial2.begin(115200, SERIAL_8N1, RXD1, TXD1);  
+  Serial1.println("Starting Arduino BLE Client application...");
+  //setup my pins
+   Setup_Potentiometer();
+  //setup BLE client
+   SetUpBLEClient();
+} // End of setup.
+
+// This is the Arduino main loop function.
+void loop() {
+  //read uART1
+  //while (Serial1.available()) {
+    //Serial.print(char(Serial1.read()));
+  //}
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are 
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    if (connectToServer()) {
+      Serial1.println("We are now connected to the BLE Server.");
+    } else {
+      Serial1.println("We have failed to connect to the server; there is nothin more we will do.");
+    }
+    doConnect = false;
+  }
+
+  // If we are connected to a peer BLE Server, update the characteristic each time we are reached
+  // with the current time since boot.
+  if (connected) {
+    MeasureValues();
+    delay(10);
+    // MotorsSpeed | MotorsStatus | Servo1 | Servo2 | Servo3|Servo4|
+    String newValue = "" + String(M_speed_value) + "|" + String(MotorsStatus) + "|" +String(S1_value)+ "|" + String(S2_value) + "|" + String(S3_value) + "|" + String(S4_value) + "|;";
+    Serial1.println("Output: \"" + newValue + "\"");
+    // Set the characteristic's value to be the array of bytes that is actually a string.
+    pRemoteCharacteristic->writeValue(newValue.c_str(), newValue.length());
+  }else if(doScan){
+    BLEDevice::getScan()->start(0);  // this is just eample to start scan after disconnect, most likely there is better way to do it in arduino
+  }
+  
+  delay(100); // Delay between loops.
+} // End of loop
